6299c94b46927b3f8bde6fbe8fe6aa5052ea88b7
hamzahaly <hzhaly93@gmail.com>
2017-03-03 16:01:50 -0800

    adds tabbar layout

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/MainActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.washington.ischool.commoncents.commoncents;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+  public static final String TAG = "TAG";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,35 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+    tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+    tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+    tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+    final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+    PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+    viewPager.setAdapter(adapter);
+    viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override
+      public void onTabSelected(TabLayout.Tab tab) {
+        Log.v(TAG, String.valueOf(tab.getPosition()));
+        viewPager.setCurrentItem(tab.getPosition());
+      }
+
+      @Override
+      public void onTabUnselected(TabLayout.Tab tab) {
+
+      }
+
+      @Override
+      public void onTabReselected(TabLayout.Tab tab) {
+
+      }
+    });
+
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
