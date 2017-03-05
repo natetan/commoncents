bd9cc6eeafb34e036f506f56ed5b9ecae25e4266
Yulong Tan <yulongtan67@gmail.com>
2017-03-05 13:41:45 -0800

    remove unused imports and add action bar to settings

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/MainActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.*;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
