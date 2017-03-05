cc69396bdcbec19f3bf7d5c57ded28ecb4a4020f
Keegan Farley <keegomyneego@live.com>
2017-03-04 18:12:33 -0800

    WIP refactoring activities and fragments

---
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/FriendProfileActivity.java
@@ -0,0 +1,15 @@
+package edu.washington.ischool.commoncents.commoncents.Activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.washington.ischool.commoncents.commoncents.R;
+
+public class FriendProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+    }
+}
---
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/SplitByItemActivity.java
@@ -0,0 +1,15 @@
+package edu.washington.ischool.commoncents.commoncents.Activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.washington.ischool.commoncents.commoncents.R;
+
+public class SplitByItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_by_item);
+    }
+}
