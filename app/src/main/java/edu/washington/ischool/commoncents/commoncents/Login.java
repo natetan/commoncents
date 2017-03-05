58cd534a3e40f4741289e03e9016c9d187c69f7a
Yulong Tan <yulongtan67@gmail.com>
2017-03-04 19:07:57 -0800

    add initial sign up and login alert frags

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Login.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Login.java
@@ -1,7 +1,16 @@
 package edu.washington.ischool.commoncents.commoncents;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.AbstractCollection;
 
@@ -10,9 +19,75 @@ import java.util.AbstractCollection;
  */
 
 public class Login extends Activity {
+
+    private Button signupButton, loginButton;
+    private String email, password;
     @Override
     protected void onCreate(Bundle onSavedInstance) {
         super.onCreate(onSavedInstance);
         setContentView(R.layout.login_activity);
+
+        Context context = getBaseContext();
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText emailEditText = new EditText(context);
+        emailEditText.setHint("email");
+        emailEditText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        layout.addView(emailEditText);
+
+        final EditText passwordEditText = new EditText(context);
+        passwordEditText.setHint("password");
+        passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        layout.addView(passwordEditText);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+
+        signupButton = (Button) findViewById(R.id.signupButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setTitle("Sign Up");
+                builder.setPositiveButton("Create Account", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        email = emailEditText.getText().toString();
+                        password = passwordEditText.getText().toString();
+                        Toast.makeText(getBaseContext(), "Email: " + email + "; Password: " +
+                            password, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            }
+        });
+        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setTitle("Log In");
+                builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        email = emailEditText.getText().toString();
+                        password = passwordEditText.getText().toString();
+                        Toast.makeText(getBaseContext(), "Email: " + email + "; Password: " +
+                            password, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            }
+        });
     }
 }
