7c6f854a0573c51675a87aa94ea8dab05cef0796
Keegan Farley <keegomyneego@live.com>
2017-03-04 19:59:11 -0800

    working on the settings page, still causing crashes

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/SettingsActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/SettingsActivity.java
@@ -15,6 +15,8 @@ public class SettingsActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
+
+    // TODO: fix this line, it's causing crashes
     getActionBar().setTitle("Settings");
 
     signupLoginButton = (Button) findViewById(R.id.signupLoginButton);
