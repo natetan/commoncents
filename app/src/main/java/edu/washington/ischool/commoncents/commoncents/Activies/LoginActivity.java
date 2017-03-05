a78985ecb4d909c81b24c7b47aa1e5fba98a2ae8
Yulong Tan <yulongtan67@gmail.com>
2017-03-05 15:55:21 -0800

    add Firebase dependency to project

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/LoginActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/LoginActivity.java
@@ -3,7 +3,6 @@ package edu.washington.ischool.commoncents.commoncents.Activies;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
