e90d7b629852c23361bc424d273b6e59f99463aa
Yulong Tan <yulongtan67@gmail.com>
2017-03-06 18:21:29 -0800

    checks if user is logged in, and does sign outs

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/LoginActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/LoginActivity.java
@@ -65,7 +65,6 @@ public class LoginActivity extends AppCompatActivity {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
-                // ...
             }
         };
 
--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Fragments/SettingsFragment.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Fragments/SettingsFragment.java
@@ -3,21 +3,34 @@ package edu.washington.ischool.commoncents.commoncents.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import edu.washington.ischool.commoncents.commoncents.Activies.LoginActivity;
 import edu.washington.ischool.commoncents.commoncents.R;
-import edu.washington.ischool.commoncents.commoncents.SettingsActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
     private Button signupLoginButton, inviteFriendsButton, changeCurrencyButton;
+
+    // Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public final String TAG = getActivity().getClass().getSimpleName();
+
+    public static final String SIGN_IN = "Sign Up / Login";
+    public static final String SIGN_OUT = "Sign out";
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -30,14 +43,38 @@ public class SettingsFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        // Initialize the Firebase auth instance
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         signupLoginButton = (Button) view.findViewById(R.id.signupLoginButton);
         inviteFriendsButton = (Button) view.findViewById(R.id.inviteFriendsButton);
         changeCurrencyButton = (Button) view.findViewById(R.id.changeCurrencyButton);
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    signupLoginButton.setText(SIGN_OUT);
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    signupLoginButton.setText(SIGN_IN);
+                }
+            }
+        };
+
         signupLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), LoginActivity.class));
+                if (signupLoginButton.getText().toString().equals(SIGN_IN)) {
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                } else {
+                    // Sign out user
+                    FirebaseAuth.getInstance().signOut();
+                }
             }
         });
 
@@ -51,12 +88,26 @@ public class SettingsFragment extends Fragment {
         changeCurrencyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
