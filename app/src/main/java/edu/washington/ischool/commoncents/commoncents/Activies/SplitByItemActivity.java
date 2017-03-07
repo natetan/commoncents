9dfd5fc673f988b162fdc7b86d2c8a5369c20797
lalinw <i.lalinw@gmail.com>
2017-03-06 21:54:33 -0800

    set up SplitByItem Activity, splitbyitem layout

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/SplitByItemActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/SplitByItemActivity.java
@@ -1,8 +1,14 @@
 package edu.washington.ischool.commoncents.commoncents.Activies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import edu.washington.ischool.commoncents.commoncents.Adapters.FriendsInEventAdapter;
 import edu.washington.ischool.commoncents.commoncents.R;
 
 public class SplitByItemActivity extends AppCompatActivity {
@@ -10,9 +16,34 @@ public class SplitByItemActivity extends AppCompatActivity {
 
     String[] dataset = {"one", "two", "third", "fourth"};
 
+    private Button btnAddFriend;
+    private Button btnAddLineItem;
+    private EditText newFriend;
+    private EditText newLineItem;
+    private TextView friendName;
+    private TextView lineItemName;
+
+    private FriendsInEventAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split_by_item);
+
+        //UI elements
+        btnAddFriend = (Button) findViewById(R.id.add_friend);
+        btnAddLineItem = (Button) findViewById(R.id.add_lineitem);
+        newFriend = (EditText) findViewById(R.id.new_friend_name);
+        newLineItem = (EditText) findViewById(R.id.new_lineitem_name);
+
+        btnAddFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nFriend = newFriend.getText().toString();
+                newFriend.setText("");
+            }
+        });
+
     }
 }
