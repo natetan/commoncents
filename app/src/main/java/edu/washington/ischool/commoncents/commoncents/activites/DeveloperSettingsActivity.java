b3afeb785289e17b3f5df82bd73f2cc9cc93221b
Keegan Farley <keegomyneego@live.com>
2017-03-08 11:49:41 -0800

    added developer settings activity

---
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/activites/DeveloperSettingsActivity.java
@@ -0,0 +1,15 @@
+package edu.washington.ischool.commoncents.commoncents.activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.washington.ischool.commoncents.commoncents.R;
+
+public class DeveloperSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_settings);
+    }
+}
