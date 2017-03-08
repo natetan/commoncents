80ade4343cf082871edc5963a7d7c8c3d4c472a3
hamzahaly <hzhaly93@gmail.com>
2017-03-08 04:51:01 -0800

    attemps event summary page

--- app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/EventSummaryActivity.java
+++ app/src/main/java/edu/washington/ischool/commoncents/commoncents/Activies/EventSummaryActivity.java
@@ -8,16 +8,27 @@ import android.support.v7.widget.RecyclerView;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import edu.washington.ischool.commoncents.commoncents.Adapters.FriendsInEventAdapter;
+import edu.washington.ischool.commoncents.commoncents.Adapters.FriendsListAdapter;
+import edu.washington.ischool.commoncents.commoncents.AppState;
+import edu.washington.ischool.commoncents.commoncents.DataRepository;
+import edu.washington.ischool.commoncents.commoncents.Models.Friend;
+import edu.washington.ischool.commoncents.commoncents.Models.LineItem;
 import edu.washington.ischool.commoncents.commoncents.R;
 
-public class EventSummaryActivity extends AppCompatActivity {
+public class EventSummaryActivity extends AppCompatActivity implements FriendsListAdapter.Listener{
 
     private Button sendSmsBtn;
     private Button finishBtn;
+    private TextView eventName;
+    private TextView eventTotal;
     private RecyclerView eventSummaryView;
-    private FriendsInEventAdapter adapter;
+    private FriendsListAdapter adapter;
+    private int total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,14 @@ public class EventSummaryActivity extends AppCompatActivity {
 
         sendSmsBtn = (Button) findViewById(R.id.send_sms_btn);
         finishBtn = (Button) findViewById(R.id.finish_btn);
+        eventName = (TextView) findViewById(R.id.event_name);
+        eventTotal = (TextView) findViewById(R.id.event_total);
+
+        eventName.setText("" + AppState.getCurrentState().getSelectedEvent().getName());
+        for (LineItem lineItem: AppState.getCurrentState().getSelectedEvent().getLineItems()) {
+            total += lineItem.getPrice() / 100.0;
+        }
+        eventTotal.setText("" + total);
 
         sendSmsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +73,15 @@ public class EventSummaryActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         eventSummaryView.setLayoutManager(layoutManager);
 
-        adapter = new FriendsInEventAdapter(R.layout.item_friend_for_event, R.id.name, R.id.amount, R.id.percentage);
+        adapter = new FriendsListAdapter(getBaseContext(), this);
         eventSummaryView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onFriendClicked(View view, Friend friend) {
+        // Go to selected friends profile page
+        AppState.getCurrentState().selectFriend(friend);
+        //SHOW LINE ITEMS, CREATE ACTIVITY FOR LINE ITEMS
+        //startActivity(new Intent(getBaseContext(), FriendProfileActivity.class));
     }
 }
